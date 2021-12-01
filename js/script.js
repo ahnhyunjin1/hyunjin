22ccb1fd54fcfc407543c7a285d83057ba614901
ahnhyunjin1 <a01020813765@gmail.com>
2021-12-01 16:29:32 +0900

    js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,26 @@
 function scrollHandler() {
-    console.log($(window).scrollTop());
-    console.log($('.about').position().top);
-
-    if ($(window).scrollTop() >= $('.about').position().top) {
-        $('.menu-right button').css('color', '#4a4a4a');
-    } else {
-        $('.menu-right button').css('color', 'white');
-    }
-
-    //세션당 애니메이션 적용
-    $('section').each(function () {
-        if ($(window).scrollTop() >= $(this).position().top) {
-            $(this).find('.vertical-center').animate({ top: '0', opacity: '1' }, 800);
-        }
-    });
+    $(window).scrollTop() >= $(".about").position().top ? ($(".menu-right button").css("color", "#4a4a4a"),
+        $(".skill").each(function () {
+            var t = $(this)
+                , o = t.find(".percentage").text();
+            t.find(".inner-bar").animate({
+                width: o
+            }, 1200)
+        })) : $(".menu-right button").css("color", "white"),
+        $("section").each(function () {
+            $(window).scrollTop() >= $(this).position().top && $(this).find(".vertical-center").animate({
+                top: "0",
+                opacity: "1"
+            }, 800)
+        })
 }
-
-$(window).on('scroll', scrollHandler);
-
-//처음 페이지가 로딩됐을때
-scrollHandler();
-
-//섹션 스크롤
-$('.menu-right button').on('click', function () {
-    var id = $(this).attr('id');
-    if (id == "about-btn") {
-        $('html, body').animate({ scrollTop: $('.about').position().top }, 1000);
-    } else if (id == "contact-btn") {
-        $('html, body').animate({ scrollTop: $('.contact').position().top }, 1000);
-    }
-});
-
-$('.vertical-center').animate({ top: '0', opacity: '1' }, 1000);
+$(window).on("scroll", scrollHandler),
+    scrollHandler(),
+    $(".menu-right button").on("click", function () {
+        var t = $(this).attr("id");
+        "about-btn" == t ? $("html, body").animate({
+            scrollTop: $(".about").position().top
+        }, 1e3) : "contact-btn" == t && $("html, body").animate({
+            scrollTop: $(".contact").position().top
+        }, 1e3)
+    });
